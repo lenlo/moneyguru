85852ff96f5f0df76db4c78c9996214687cddf3a
Lennart Lövstrand <lennart.github@lovstrand.com>
2017-01-31 23:46:17 -0800

    Removed unneeded import of sys

--- core/gui/date_widget.py
+++ core/gui/date_widget.py
@@ -6,8 +6,6 @@
 # which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
-import sys
-
 from datetime import date
 
 from ..model.date import format_year_month_day, inc_day, inc_month, inc_year, DateFormat
